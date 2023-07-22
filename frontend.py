2cf2edbc3d9efe96c10b7ee77d90694820bf93c7
ashishkain99 <ashish.kumarkain1999@gmail.com>
2023-07-22 15:31:44 +0530

    for testing

---
+++ frontend.py
@@ -0,0 +1,14 @@
+<form action="http://13.232.176.125/cgi-bin/cmd.py">
+
+
+Command <input name="A" >
+<br>
+<input type="submit">
+</br>
+</br>
+
+Search what you want <input name="B">
+</br>
+<input type="submit">
+
+
