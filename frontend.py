d2eaa505e6b535f557bb71afcd109a05a0c40d17
Ashish kumar kain <137259625+ashishkain99@users.noreply.github.com>
2023-07-23 02:33:10 +0530

    frontend.html

--- frontend.py
+++ frontend.py
@@ -1,14 +1,9 @@
-<form action="http://52.63.59.230/cgi-bin/cmd.py">
+<form action="http://13.232.110.153/cgi-bin/cmd.py">
 
 
 Command <input name="A" >
 <br>
 <input type="submit">
-</br>
-</br>
 
-Search what you want <input name="B">
-</br>
-<input type="submit">
 
 
